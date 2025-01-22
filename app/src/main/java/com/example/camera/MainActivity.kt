6d4a9641345683e4ad24eeb835c76c71860d90e3
MagnusKomet <ehosta.dam@institutcampalans.net>
2025-01-22 18:23:46 +0100

    Initial commit

---
+++ app/src/main/java/com/example/camera/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.camera
+
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.camera.databinding.ActivityMainBinding
+import java.io.File
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener{
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE).also{
+                it.resolveActivity(packageManager).also{component->
+                    //File pot ser un fitxer emmagatzemat a la memòria, no cal que estigui al magatzem del dispositiu
+                    //val photoFile:File
+
+                    //Crearem un métode que guardi el File que necessitem
+
+                    createPhotoFile()
+
+                    //Uri sí que queda emmagatzemat a una ruta del magatzem del dispositiu
+                    val photoUri: Uri = FileProvider.getUriForFile(this,"com.example.camera.fileprovider", file)
+
+                    it.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+                    //Hem reanomenat l'iterador per defecte a component per poder continuar tinguen accés a l'iterador it que fa referència a l'intent. Sinó no ens deixaria
+                }
+            }
+            //Ara cridarem el launch passant el l'intent modificat
+            startForResult.launch(intent)
+        }
+    }
+
+    private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+    {
+            result: ActivityResult ->
+        if(result.resultCode == Activity.RESULT_OK)
+        {
+            val intent = result.data
+            //val imageBitmap = intent?.extras?.get("data") as Bitmap
+            val imageBitmap = BitmapFactory.decodeFile(file.toString())
+            val imageView = binding.imageView
+            imageView.setImageBitmap(imageBitmap)
+        }
+    }
+
+    private lateinit var file: File
+    private fun createPhotoFile() {
+        //Necessitem accedir a un directori extern
+        //Enviroment.DIRECTORY_PICTURES retorna la ruta on es guarden les images al dispositiu
+        val dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+
+        //Crearem un fitxer temporal
+        //El nom del fitxer serà "IMG_" seguit del temps actual en milisegons acabat en _. Ho indiquem al prefix:
+        //L'extensió l'indicarem al "sufix" i serà -jpg
+
+        file = File.createTempFile("ERNEST_${System.currentTimeMillis()}_",".png", dir)
+    }
+
+}
